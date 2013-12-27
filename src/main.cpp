73c298554da24a126693ee8f724c69f7185c577a
Skylar Kelty <skylarkelty@gmail.com>
2013-12-27 20:23:05 +0000

    Simple start

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/**
+ * An example simulation to show how something
+ * like this might work one day...
+ *
+ * @author Skylar Kelty <skylarkelty@gmail.com>
+ */
+
+/**
+ * The main class is used to initialize the engine
+ */
+int main( int argc, const char* argv[] ) {
+	return 0;
+}
